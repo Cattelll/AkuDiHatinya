2f12a650203c722c6cb1a2005965ca9b6f24a1a6
Cattelll <bhimonw01@gmail.com>
2025-03-05 15:03:25 +0800

    Initial commit

--- app/Views/login.php
+++ app/Views/login.php
@@ -4,22 +4,15 @@
 <head>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta
-    name="viewport"
-    content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
   <meta name="description" content="" />
   <meta name="author" content="" />
 
   <title>Login</title>
 
   <!-- Custom fonts for this template-->
-  <link
-    href="assets/login/vendor/fontawesome-free/css/all.min.css"
-    rel="stylesheet"
-    type="text/css" />
-  <link
-    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-    rel="stylesheet" />
+  <link href="assets/login/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css" />
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
 
   <!-- Custom styles for this template-->
   <link href="assets/login/css/sb-admin-2.min.css" rel="stylesheet" />
@@ -29,61 +22,57 @@
 <body class="bg-gradient-primary">
   <div class="container">
     <!-- Outer Row -->
-    <div class="row justify-content-center">
+    <div class="row justify-content-center align-items-center">
       <div class="col-xl-10 col-lg-12 col-md-9">
-        <div class="card o-hidden border-0 shadow-lg my-5">
+        <div class="card o-hidden border-0 shadow-lg">
           <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
             <div class="row">
+              <!-- Kiri: Logo, Nama Aplikasi, dan Gambar Login -->
               <div class="col-lg-6 d-none d-lg-block bg-login-image">
                 <div class="container-flex">
-                  <img src="assets/img/logo.png" alt="Logo Aplikasi" class="logo">
-                  <div class="text">
-                    <h1 class="h6">akudihatinya</h1>
-                    <h1 class="h7">Aplikasi Kesehatan untuk Diabetes Mellitus</h1>
-                    <h1 class="h7">dan Hipertensi Terlayani</h1>
+                  <div class="logo-text-wrapper">
+                    <img src="assets/login/img/logo.png" alt="Logo Aplikasi" class="logo">
+                    <div class="text">
+                      <h1 class="h6">akudihatinya</h1>
+                      <h1 class="h7">Aplikasi Kesehatan untuk Diabetes Mellitus dan Hipertensi Terlayani</h1>
+                    </div>
                   </div>
                 </div>
+                <div class="img-container">
+                  <img src="assets/login/img/login.webp" alt="Gambar Login" class="bottom-img">
+                </div>
               </div>
-              <div class="col-lg-6">
-                <div class="p-5">
+
+              <!-- Kanan: Form Login -->
+              <div class="col-lg-6 login-container d-flex align-items-center">
+                <div class="p-5 form-container w-100">
                   <div class="text-center">
                     <h1 class="h4 mb-4">Login</h1>
                   </div>
                   <form class="user">
-                    <div class="form-group">
-                      <input
-                        type="username"
-                        class="form-control form-control-user"
-                        id="usernameInput"
-                        aria-describedby="usernameHelp"
-                        placeholder="Username" />
+                    <div class="form-group input-group-custom">
+                      <input type="text" class="form-control form-control-user" id="usernameInput" placeholder="Username" required />
+                      <img src="assets/login/img/username.png" class="input-icon" alt="User Icon">
                     </div>
-                    <div class="form-group">
-                      <input
-                        type="password"
-                        class="form-control form-control-user"
-                        id="exampleInputPassword"
-                        placeholder="Password" />
+
+                    <div class="form-group input-group-custom">
+                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password" required />
+                      <img src="assets/login/img/passwordIcon.jpg" class="input-icon" alt="Lock Icon">
                     </div>
+                    <!-- Remember Me Checkbox -->
                     <div class="form-group">
                       <div class="custom-control custom-checkbox small">
-                        <input
-                          type="checkbox"
-                          class="custom-control-input"
-                          id="customCheck" />
+                        <input type="checkbox" class="custom-control-input" id="customCheck" />
                         <label class="custom-control-label" for="customCheck">Remember Me</label>
                       </div>
                     </div>
-                    <a
-                      href="index.html"
-                      class="btn btn-primary btn-user btn-block">
-                      Login
-                    </a>
+                    <!-- Tombol Login -->
+                    <a href="dashboard" class="btn btn-primary btn-user btn-block">Login</a>
                   </form>
                 </div>
               </div>
-            </div>
+            </div> <!-- End row -->
           </div>
         </div>
       </div>
