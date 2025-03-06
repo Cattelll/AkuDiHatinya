1fd96830f528a35db25a16d21116d9a62d97e509
Cattelll <bhimonw01@gmail.com>
2025-03-06 09:55:25 +0800

    Initial commit

--- app/Views/login.php
+++ app/Views/login.php
@@ -28,62 +28,57 @@
 
 <body class="bg-gradient-primary">
   <div class="container">
+
     <!-- Outer Row -->
-    <div class="row justify-content-center">
+    <div class="row justify-content-center align-items-center">
       <div class="col-xl-10 col-lg-12 col-md-9">
-        <div class="card o-hidden border-0 shadow-lg my-5">
+        <div class="card o-hidden border-0 shadow-lg">
           <div class="card-body p-0">
+
             <!-- Nested Row within Card Body -->
             <div class="row">
+
+              <!-- Kiri: Logo, Nama Aplikasi, dan Gambar Login -->
               <div class="col-lg-6 d-none d-lg-block bg-login-image">
                 <div class="container-flex">
-                  <img src="assets/img/logo.png" alt="Logo Aplikasi" class="logo">
-                  <div class="text">
-                    <h1 class="h6">akudihatinya</h1>
-                    <h1 class="h7">Aplikasi Kesehatan untuk Diabetes Mellitus</h1>
-                    <h1 class="h7">dan Hipertensi Terlayani</h1>
+                  <div class="logo-text-wrapper">
+                    <img src="assets/login/img/logo.png" alt="Logo Aplikasi" class="logo">
+                    <div class="text">
+                      <h1 class="h6">akudihatinya</h1>
+                      <h1 class="h7">Aplikasi Kesehatan untuk Diabetes Mellitus dan Hipertensi Terlayani</h1>
+                    </div>
                   </div>
                 </div>
+                <div class="img-container">
+                  <img src="assets/login/img/login.webp" alt="Gambar Login" class="bottom-img">
+                </div>
               </div>
-              <div class="col-lg-6">
-                <div class="p-5">
+
+              <!-- Kanan: Form Login -->
+              <div class="col-lg-6 login-container d-flex align-items-center">
+                <div class="p-5 form-container w-100">
                   <div class="text-center">
                     <h1 class="h4 mb-4">Login</h1>
                   </div>
                   <form class="user">
                     <div class="form-group">
-                      <input
-                        type="username"
-                        class="form-control form-control-user"
-                        id="usernameInput"
-                        aria-describedby="usernameHelp"
-                        placeholder="Username" />
+                      <input type="text" class="form-control form-control-user" id="usernameInput" placeholder="Username" />
                     </div>
                     <div class="form-group">
-                      <input
-                        type="password"
-                        class="form-control form-control-user"
-                        id="exampleInputPassword"
-                        placeholder="Password" />
+                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password" />
                     </div>
                     <div class="form-group">
                       <div class="custom-control custom-checkbox small">
-                        <input
-                          type="checkbox"
-                          class="custom-control-input"
-                          id="customCheck" />
+                        <input type="checkbox" class="custom-control-input" id="customCheck" />
                         <label class="custom-control-label" for="customCheck">Remember Me</label>
                       </div>
                     </div>
-                    <a
-                      href="index.html"
-                      class="btn btn-primary btn-user btn-block">
-                      Login
-                    </a>
+                    <a href="index.html" class="btn btn-primary btn-user btn-block">Login</a>
                   </form>
                 </div>
               </div>
-            </div>
+
+            </div> <!-- End row -->
           </div>
         </div>
       </div>
